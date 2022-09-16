3de7c9572ff4ad1d146e76e41bdb5c80dfd807a9
zibin.liu <zibin.liu@okg.com>
2022-09-16 11:05:43 +0800

    feat: add Nutui

--- config/index.js
+++ config/index.js
@@ -1,35 +1,42 @@
 const config = {
-  projectName: 'hula',
-  date: '2022-9-16',
+  projectName: "hula",
+  date: "2022-9-16",
   designWidth: 750,
   deviceRatio: {
     640: 2.34 / 2,
     750: 1,
     828: 1.81 / 2
   },
-  sourceRoot: 'src',
-  outputRoot: 'dist',
-  plugins: [],
-  defineConstants: {
-  },
+  sourceRoot: "src",
+  outputRoot: "dist",
+
+  defineConstants: {},
   copy: {
-    patterns: [
-    ],
-    options: {
-    }
+    patterns: [],
+    options: {}
   },
-  framework: 'vue3',
-  compiler: 'webpack5',
+  framework: "vue3",
+  compiler: "webpack5",
   cache: {
     enable: false // Webpack 持久化缓存配置，建议开启。默认配置请参考：https://docs.taro.zone/docs/config-detail#cache
+  },
+  // 配置nutui-taro
+  plugins: ["@tarojs/plugin-html"],
+  sass: {
+    data: `@import "@nutui/nutui-taro/dist/styles/variables.scss";`
+  },
+  designWidth: 375,
+  deviceRatio: {
+    640: 2.34 / 2,
+    750: 1,
+    828: 1.81 / 2,
+    375: 2 / 1
   },
   mini: {
     postcss: {
       pxtransform: {
         enable: true,
-        config: {
-
-        }
+        config: {}
       },
       url: {
         enable: true,
@@ -40,43 +47,42 @@ const config = {
       cssModules: {
         enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
         config: {
-          namingPattern: 'module', // 转换模式，取值为 global/module
-          generateScopedName: '[name]__[local]___[hash:base64:5]'
+          namingPattern: "module", // 转换模式，取值为 global/module
+          generateScopedName: "[name]__[local]___[hash:base64:5]"
         }
       }
     }
   },
   h5: {
-    publicPath: '/',
-    staticDirectory: 'static',
+    publicPath: "/",
+    staticDirectory: "static",
     postcss: {
       autoprefixer: {
         enable: true,
-        config: {
-        }
+        config: {}
       },
       cssModules: {
         enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
         config: {
-          namingPattern: 'module', // 转换模式，取值为 global/module
-          generateScopedName: '[name]__[local]___[hash:base64:5]'
+          namingPattern: "module", // 转换模式，取值为 global/module
+          generateScopedName: "[name]__[local]___[hash:base64:5]"
         }
       }
     }
   },
   rn: {
-    appName: 'taroDemo',
+    appName: "taroDemo",
     postcss: {
       cssModules: {
-        enable: false, // 默认为 false，如需使用 css modules 功能，则设为 true
+        enable: false // 默认为 false，如需使用 css modules 功能，则设为 true
       }
     }
   }
-}
+};
 
-module.exports = function (merge) {
-  if (process.env.NODE_ENV === 'development') {
-    return merge({}, config, require('./dev'))
+module.exports = function(merge) {
+  if (process.env.NODE_ENV === "development") {
+    return merge({}, config, require("./dev"));
   }
-  return merge({}, config, require('./prod'))
-}
+  return merge({}, config, require("./prod"));
+};
--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
-import { createApp } from 'vue'
-import './app.scss'
+import { createApp } from "vue";
+import { Button } from "@nutui/nutui-taro";
+import "./app.scss";
 
 const App = createApp({
-  onShow (options) {},
-  // 入口组件不需要实现 render 方法，即使实现了也会被 taro 所覆盖
-})
+  onShow(options) {}
+});
 
-export default App
+App.use(Button);
+
+export default App;
